c909dc7ba3bdabcf2d0fdf79ac1af0b3812d6edd
Andi Muhammad Raihan Syamsu <amuhraihan2324@gmail.com>
2025-06-13 01:38:19 +0700

    update list item(tampilan)

--- app/src/main/java/com/andimuhammadraihansyamsu607062330113/mybookassess3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/andimuhammadraihansyamsu607062330113/mybookassess3/ui/screen/MainScreen.kt
@@ -18,9 +18,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -41,7 +44,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -305,18 +307,17 @@ fun ListItem(buku: Buku, onClick: () -> Unit) {
         else -> Color.Gray
     }
 
-    Box(
+    Card(
         modifier = Modifier
             .padding(8.dp)
-            .background(MaterialTheme.colorScheme.surface, shape = MaterialTheme.shapes.medium)
-            .shadow(8.dp, shape = MaterialTheme.shapes.medium, clip = false)
-            .clickable(onClick = onClick)
-            .padding(8.dp)
+            .clickable(onClick = onClick),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.White, shape = MaterialTheme.shapes.medium)
                 .clip(MaterialTheme.shapes.medium)
         ) {
             AsyncImage(
@@ -331,23 +332,22 @@ fun ListItem(buku: Buku, onClick: () -> Unit) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(180.dp)
-                    .clip(MaterialTheme.shapes.medium)
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
             )
 
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0f, 0f, 0f, 0.5f))
                     .padding(8.dp)
-                    .clip(MaterialTheme.shapes.medium)
             ) {
                 Text(
                     text = buku.judul,
                     fontWeight = FontWeight.Bold,
                     fontSize = 16.sp,
-                    color = Color.White,
+                    color = MaterialTheme.colorScheme.onSurface,
                     modifier = Modifier.padding(bottom = 4.dp)
                 )
+
                 Text(
                     text = buku.status,
                     fontStyle = FontStyle.Italic,
@@ -362,6 +362,7 @@ fun ListItem(buku: Buku, onClick: () -> Unit) {
         }
     }
 }
+
 
 private suspend fun signIn(context : Context, dataStore: UserDataStore) {
     val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
